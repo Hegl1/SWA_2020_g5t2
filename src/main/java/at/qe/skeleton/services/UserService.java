0a53b0652c4691889b03e33904f97845c30795cc
csar9824 <philip.eckhart@student-uibk.ac.at>
2020-12-19 14:52:49 +0100

    Updated UserService.java

--- src/main/java/at/qe/skeleton/services/UserService.java
+++ src/main/java/at/qe/skeleton/services/UserService.java
@@ -2,6 +2,7 @@ package at.qe.skeleton.services;
 
 import java.sql.SQLOutput;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -37,7 +38,7 @@ public class UserService {
 	 *
 	 * @return
 	 */
-	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('LIBRARIAN')")
+	// @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('LIBRARIAN')")
 	public Collection<User> getAllUsers() {
 		return userRepository.findAll();
 	}
@@ -53,15 +54,15 @@ public class UserService {
 		return userRepository.findFirstByUsername(username);
 	}
 
-    @PreAuthorize("hasAuthority('ADMIN')")
-    public List<User> loadUserByName(String fullName) {
-        return userRepository.findByWholeNameConcat(fullName);
-    }
-    
-    public User loadCurrentUser(){
-    	return loadUser(getAuthenticatedUser().getUsername());
-    }
-	
+	@PreAuthorize("hasAuthority('ADMIN')")
+	public List<User> loadUserByName(String fullName) {
+		return userRepository.findByWholeNameConcat(fullName);
+	}
+
+	public User loadCurrentUser() {
+		return loadUser(getAuthenticatedUser().getUsername());
+	}
+
 	/**
 	 * Saves the user. This method will also set {@link User#createDate} for new
 	 * entities or {@link User#updateDate} for updated entities. The user requesting
@@ -72,38 +73,65 @@ public class UserService {
 	 * @return the updated user
 	 */
 	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('LIBRARIAN')")
-	public User saveUser(final User user) {
+	public User saveUser(User user) {
+		if (user.isNew()) {
+			user.setCreateDate(new Date());
+			user.setCreateUser(getAuthenticatedUser());
+		} else {
+			user.setUpdateDate(new Date());
+			user.setUpdateUser(getAuthenticatedUser());
+		}
 		return userRepository.save(user);
 	}
-	
-    /**
-     * Changes the set of roles a user possesses.
-     * @param user The user whose roles should be changed
-     * @param newRoles Set of new roles
-     * @return if the change was succesful.
-     */
-    @PreAuthorize("hasAuthority('ADMIN')")
-    public boolean changeUserRoles(User user, UserRole newRoles)
-    {
-    	try
-    	{
-    		user.setRoles(newRoles);
-    	}
-    	catch(Exception e)
-    	{
-    		return false;
-    	}
-    	return true;
-    }
+
+	/**
+	 * Changes the set of roles a user possesses.
+	 * 
+	 * @param user     The user whose roles should be changed
+	 * @param newRoles Set of new roles
+	 * @return if the change was succesful.
+	 */
+	@PreAuthorize("hasAuthority('ADMIN')")
+	public boolean changeUserRoles(User user, Set<UserRole> newRoles) {
+		try {
+			user.setRoles(newRoles);
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean changeUserRoles(User user, List<String> newRolesString) {
+		Set<UserRole> newRolesSet = new HashSet<>();
+
+		for (String selected : newRolesString) {
+			switch (selected.toLowerCase()) {
+			case "librarian":
+				newRolesSet.add(UserRole.LIBRARIAN);
+				break;
+			case "admin":
+				newRolesSet.add(UserRole.ADMIN);
+				break;
+			default:
+				System.err.println(
+						"[Warning] UserService - changeUserRoles: Role \"" + selected + "\" not supported yet!");
+				return false;
+			}
+		}
+		user.setRoles(newRolesSet);
+
+		return true;
+	}
 
 	/**
 	 * Creates a user.
 	 */
 	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('LIBRARIAN')")
-	public User createUser(String username, String password, String firstName, String lastName, Boolean enabled, UserRole roles, String email) throws UnauthorizedActionException {
+	public User createUser(String username, String password, String firstName, String lastName, Boolean enabled,
+			UserRole roles, String email) throws UnauthorizedActionException {
 
-		if(this.getAuthenticatedUser().getRoles().contains(UserRole.LIBRARIAN) &&
-				(roles.equals(UserRole.LIBRARIAN) || roles.equals(UserRole.ADMIN))) {
+		if (this.getAuthenticatedUser().getRoles().contains(UserRole.LIBRARIAN)
+				&& (roles.equals(UserRole.LIBRARIAN) || roles.equals(UserRole.ADMIN))) {
 			throw new UnauthorizedActionException("Librarians may not create Admins!");
 		}
 
@@ -118,14 +146,23 @@ public class UserService {
 	 *
 	 * @param user the user to delete
 	 */
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+        // :TODO: write some audit log stating who and when this user was permanently
+        // deleted.
+    }
+/*	
 	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('LIBRARIAN')")
 	public void deleteUser(final User user) throws UnauthorizedActionException {
 
-		// TODO: potential issue, that an Admin has less rights if he has the Librarian-Role as well
-		// this problem should not occur, if only one Role is allowed in the Constructor and Setter of the User
+		// TODO: potential issue, that an Admin has less rights if he has the
+		// Librarian-Role as well
+		// this problem should not occur, if only one Role is allowed in the Constructor
+		// and Setter of the User
 
-		if(this.getAuthenticatedUser().getRoles().contains(UserRole.LIBRARIAN) &&
-				user.getRoles().contains(UserRole.ADMIN)) {
+		if (this.getAuthenticatedUser().getRoles().contains(UserRole.LIBRARIAN)
+				&& user.getRoles().contains(UserRole.ADMIN)) {
 
 			throw new UnauthorizedActionException("Librarian may not delete Administrators!");
 
@@ -136,7 +173,7 @@ public class UserService {
 		} else {
 			userRepository.delete(user);
 		}
-	}
+	}*/
 
 	private User getAuthenticatedUser() {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -146,7 +183,7 @@ public class UserService {
 	public static class UnauthorizedActionException extends Exception {
 		private static final long serialVersionUID = 1L;
 
-		public UnauthorizedActionException(String message){
+		public UnauthorizedActionException(String message) {
 			super(message);
 		}
 	}
