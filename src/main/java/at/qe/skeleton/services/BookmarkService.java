ed8de5ee2a1bdd84802c02295d616be21d50dae5
csaw4579 <Marcel.Huber@student.uibk.ac.at>
2021-01-03 11:07:29 +0100

    Refeactoring of bookmarking system

--- src/main/java/at/qe/skeleton/services/BookmarkService.java
+++ src/main/java/at/qe/skeleton/services/BookmarkService.java
@@ -2,15 +2,14 @@ package at.qe.skeleton.services;
 
 import java.util.Collection;
 
-import at.qe.skeleton.model.Book;
-import at.qe.skeleton.model.Media;
-import at.qe.skeleton.model.User;
-import at.qe.skeleton.repositories.BookmarkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import at.qe.skeleton.model.Bookmark;
+import at.qe.skeleton.model.Media;
+import at.qe.skeleton.model.User;
+import at.qe.skeleton.repositories.BookmarkRepository;
 
 /**
  * Service for listing the customers own bookmarks.
@@ -20,106 +19,112 @@ import at.qe.skeleton.model.Bookmark;
 @Scope("application")
 public class BookmarkService {
 
-    @Autowired
-    private BookmarkRepository bookmarkRepository;
+	@Autowired
+	private BookmarkRepository bookmarkRepository;
 
-    @Autowired
-    private MediaService mediaService_2;
+	@Autowired
+	private UserService userService;
 
-    @Autowired
-    private UserService userService;
+	/**
+	 * Returns a collection of all the bookmarks.
+	 * 
+	 * @return all bookmarks in the database
+	 */
+	public Collection<Bookmark> getAllBookmarks() {
+		return bookmarkRepository.findAll();
+	}
 
-    /**
-     * Returns a collection of all the bookmarks.
-     * @return all bookmarks in the database
-     */
-    public Collection<Bookmark> getAllBookmarks() { return bookmarkRepository.findAll(); }
+	/**
+	 * Returns a collection of the current user bookmarks.
+	 *
+	 * @return only the bookmarks which belong to the current logged in users
+	 * @param user
+	 */
+	public Collection<Bookmark> getBookmarksByUser(final User user) {
 
-    /**
-     * Returns a collection of the current user bookmarks.
-     *
-     * @return only the bookmarks which belong to the current logged in users
-     * @param myCurrentUser
-     */
-    public Collection<Bookmark> getMyBookmarks(User myCurrentUser) {
+		return bookmarkRepository.findByUsername(user.getUsername());
+	}
 
-        return bookmarkRepository.findMine(myCurrentUser.getUsername());}
+	/**
+	 * Returns 1 bookmark according to a given ID.
+	 *
+	 * @param bookmarkID of the wanted bookmark
+	 * @return the bookmark with exact this ID
+	 */
+	public Bookmark loadBookmark(final Long bookmarkID) {
+		return bookmarkRepository.findFirstByBookmarkID(bookmarkID);
+	}
 
-    /**
-     * Returns 1 bookmark according to a given ID.
-     *
-     * @param bookmarkID of the wanted bookmark
-     * @return the bookmark with exact this ID
-     */
-    public Bookmark loadBookmark(final Long bookmarkID ) { return bookmarkRepository.findFirstByBookmarkID(bookmarkID);}
+	/**
+	 * the following 4 functions return type specific information about the media
+	 */
+	public String getMediaTypeAsString(final Bookmark bookmark) {
+		return bookmark.getMedia().getMediaType().toString();
+	}
 
-    /**
-     * the following 4 functions return type specific information about the media
-     * */
-    public String getThatType(final Bookmark bookmark) {   return bookmark.getMedia().getMediaType().toString(); }
+	public String getMediaTitle(final Bookmark bookmark) {
+		return bookmark.getMedia().getTitle();
+	}
 
-    public String getThatTitle(final Bookmark bookmark) {   return bookmark.getMedia().getTitle(); }
+	public String getMediaInfo(final Bookmark bookmark) {
 
-    public String getThatAddInfo(final Bookmark bookmark) {
+		switch (bookmark.getMedia().getMediaType().toString()) {
+		case "BOOK":
+			return "Book - Author";
 
-        switch (bookmark.getMedia().getMediaType().toString()) {
-            case "BOOK":
-                return "Buch - Author";
+		case "AUDIOBOOK":
+			return "AudioBook - Author";
 
-            case "AUDIOBOOK":
-                return "AudioBuch - Author";
+		case "MAGAZINE":
+			return "Magazine - Serie";
 
-            case "MAGAZINE":
-                return "Magazin - Serie";
+		case "VIDEO":
+			return "Video - Länge";
 
-            case "VIDEO":
-                return "Video - Länge";
+		default:
+			return "non defined media type";
+		}
+	}
 
-            default:
-                return "nicht definierter Medientyp";
-        }
-    }
+	public String getIfCurrentBorrowed(final Bookmark bookmark) {
 
-    public String getIfCurrentBorrowed(final Bookmark bookmark) {
+		switch (bookmark.getMedia().getCurBorrowed()) {
+		case 0:
+			return "0 - free";
 
-        switch (bookmark.getMedia().getCurBorrowed()) {
-            case 0:
-                return "0 - frei";
+		case 1:
+			return "1 - occupied";
 
-            case 1:
-                return "1 - ausgeliehen";
+		default:
+			return "no status available";
+		}
+	}
 
-            default:
-                return "Status nicht verfügbar";
-        }
-    }
+	/**
+	 * Deletes the bookmark.
+	 *
+	 * @param bookmark the bookmark to delete
+	 */
+	public void deleteBookmark(final Bookmark bookmark) {
 
+		bookmarkRepository.delete(bookmark);
+	}
 
+	/**
+	 * add a bookmark.
+	 *
+	 * @param media the media to add as one's own bookmark
+	 */
+	public void addBookmark(final Media media) {
+		User myUser = userService.loadCurrentUser();
+		Bookmark b_check = bookmarkRepository.findFirstByMedia(media);
 
-    /**
-     * Deletes the bookmark.
-     *
-     * @param bookmark the bookmark to delete
-     */
-    public void deleteBookmark(Bookmark bookmark) {
-
-            bookmarkRepository.delete(bookmark);
-        }
-
-    /**
-     * add a bookmark.
-     *
-     * @param media the media to add as one's own bookmark
-     */
-    public void addBookmark(Media media) {
-        User myUser = userService.loadCurrentUser();
-        Bookmark b_check = bookmarkRepository.findFirstByMedia(media);
-
-        if (b_check == null) {
-            bookmarkRepository.add(media, myUser.getUsername());
-        }
-    }
-
-
+		if (b_check == null) {
+			Bookmark mark = new Bookmark();
+			mark.setMedia(media);
+			mark.setUser(myUser);
+			bookmarkRepository.save(mark);
+		}
+	}
 
 }
--- src/main/java/at/qe/skeleton/ui/controllers/BookmarkListController.java
+++ src/main/java/at/qe/skeleton/ui/controllers/BookmarkListController.java
@@ -1,16 +1,16 @@
 package at.qe.skeleton.ui.controllers;
 
-import at.qe.skeleton.model.Bookmark;
-import at.qe.skeleton.model.User;
-import at.qe.skeleton.services.BookmarkService;
 import java.io.Serializable;
 import java.util.Collection;
-
-import at.qe.skeleton.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import at.qe.skeleton.model.Bookmark;
+import at.qe.skeleton.model.User;
+import at.qe.skeleton.services.BookmarkService;
+import at.qe.skeleton.services.UserService;
 
 /**
  * Controller for the bookmark list view.
@@ -21,39 +21,32 @@ import org.springframework.stereotype.Component;
 
 public class BookmarkListController implements Serializable {
 
-    @Autowired
-    private BookmarkService bookmarkService;
+	@Autowired
+	private BookmarkService bookmarkService;
 
-    private Bookmark bookmark;
+	private Bookmark bookmark;
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    /**
-     * Returns a list of the current customers bookmarks.
-     *
-     * @return all bookmarks of the logged in user
-     */
-    public Collection<Bookmark> getBookmarks() {
-        User myCurrentUser = userService.loadCurrentUser();
-        
-        return bookmarkService.getMyBookmarks(myCurrentUser);
-    }
+	/**
+	 * Returns a list of the current customers bookmarks.
+	 *
+	 * @return all bookmarks of the logged in user
+	 */
+	public Collection<Bookmark> getBookmarks() {
+		User currentUser = userService.loadCurrentUser();
 
+		return bookmarkService.getBookmarksByUser(currentUser);
+	}
 
-    /**
-     * Action to delete the currently displayed bookmark.
-     */
-    public void doDeleteBookmark()  {
+	/**
+	 * Action to delete the currently displayed bookmark.
+	 */
+	public void doDeleteBookmark() {
 
-        this.bookmarkService.deleteBookmark(bookmark);
-        bookmark = null;
-    }
-
-
+		this.bookmarkService.deleteBookmark(bookmark);
+		bookmark = null;
+	}
 
 }
-
-
-
-
