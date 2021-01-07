977b2d5a593e4208239dd16548cf8139523a0dcf
SpleitDave <SpleitDave@users.noreply.github.com>
2021-01-07 21:43:48 +0100

    delete-loading-dialog + notify users via Mail when their bookmarked media and bookmark was deleted + setting that borrowed media cannot be deleted #9

--- src/main/java/at/qe/skeleton/ui/controllers/MediaDetailController.java
+++ src/main/java/at/qe/skeleton/ui/controllers/MediaDetailController.java
@@ -1,9 +1,12 @@
 package at.qe.skeleton.ui.controllers;
 
 import at.qe.skeleton.model.*;
+import at.qe.skeleton.repositories.BookmarkRepository;
 import at.qe.skeleton.services.BookmarkService;
 import at.qe.skeleton.services.BorrowService;
+import at.qe.skeleton.services.MailService;
 import at.qe.skeleton.services.MediaService;
+import org.primefaces.PrimeFaces;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -32,6 +35,12 @@ public class MediaDetailController implements Serializable {
 
     @Autowired
     BookmarkService bookmarkService;
+
+    @Autowired
+    MailService mailservice;
+
+    @Autowired
+    BookmarkRepository bookmarkRepository;
 
 
     /**
@@ -104,55 +113,66 @@ public class MediaDetailController implements Serializable {
         // this.doReloadMedia();
     }
 
+    public void orderDeleteMedia(final Media media) {
+
+        // a loading message is shown, as the deleting and mail sending process can take up to 20 seconds
+        doDeleteMedia(media);
+        PrimeFaces.current().executeScript("PF('dataChangeDlg').hide()");
+    }
 
     /**
      * Action to delete the currently displayed media.
      */
     public void doDeleteMedia(final Media media) {
+
+        // Step 1: Check if the media is still borrowed
         FacesContext context = FacesContext.getCurrentInstance();
         Collection<Borrowed> a1 = borrowService.getAllBorrowsByMedia(media);
         if (a1.size() > 0){
-            System.out.println("\nThis media is still borrowed by x people: " + a1.size());
+            System.out.println("\nThis media is still borrowed by x " + a1.size()+" people.");
             for (Borrowed a : a1) {
                 System.out.println("  by: " + a.getUser());
-                context.addMessage("asGrowl", new FacesMessage(FacesMessage.SEVERITY_WARN, "This media is still borrowed by : " + a.getUser().getUsername(), ""));
+                context.addMessage("asGrowl", new FacesMessage(FacesMessage.SEVERITY_WARN, "This media is still borrowed by : " + a.getUser().getUsername() + " and cannot be deleted.", ""));
             }
         } else {
             System.out.println("\nnobody is borrowing it");
         }
 
+        // Step 2: Get a list of users that bookmarked this media eventually
         Collection<Bookmark> a2 = bookmarkService.getAllBookmarks();
         List<User> a2_s = new ArrayList<User>();
-
-
         for (Bookmark b : a2)
             if(b.getMedia().getMediaID() == media.getMediaID())
                 a2_s.add(b.getUser());
 
+        // Step 3: If the media is not borrowed and only bookmarked: Delete the media and bookmark entries and notify users
+        int MediaAlreadyDeleted;
+        MediaAlreadyDeleted = 0;
         if (a2_s.size() > 0){
+
             System.out.println("\nThis media is still bookmarked by some people: ");
+
             for (User u : a2_s){
                 System.out.println("  by: "+ u.getUsername());
                 context.addMessage("asGrowl", new FacesMessage(FacesMessage.SEVERITY_WARN, "This media is still bookmarked by: "+ u.getUsername(),  "") );
+                bookmarkService.deleteBookmark(bookmarkRepository.findFirstByUserAndMedia(u, media));
+                mailservice.sendMail(u.getEmail(), "> The Media of your Bookmark was deleted", "The following Media is not available anymore: Title: "+ media.getTitle() +", Type: "+media.getMediaType()+", Language: "+media.getLanguage()+", Publishing Year: "+media.getPublishingYear());
             }
+            this.mediaService.deleteMedia(media);
+            this.media = null;
 
+            System.out.println("\nMedia was deleted finally.");
+            MediaAlreadyDeleted = 1;
 
         } else {
             System.out.println("\nnobody is bookmarking it");
         }
 
-        // only delete if it is not borrowed or bookmarked
-        if (!(a1.size() > 0) && !(a2_s.size() > 0)){
+        // Step 4: Delete the media only if it has not been borrowed by somebody and was not deleted in step 3
+        if ((!(a1.size() > 0) && (MediaAlreadyDeleted==0)) ){
             this.mediaService.deleteMedia(media);
             this.media = null;
         }
 
-
     }
-
-
-
-
-
-
 }
