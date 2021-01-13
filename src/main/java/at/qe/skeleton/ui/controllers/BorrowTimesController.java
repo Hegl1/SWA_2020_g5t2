19570aec30cfb6d78613e711c0e0178e1a760e75
csaw4579 <Marcel.Huber@student.uibk.ac.at>
2021-01-13 15:43:35 +0100

    #44 add ability to undo/redo editings of borrow times
    removed the ability to undo/redo bookmarks

--- src/main/java/at/qe/skeleton/ui/controllers/BorrowTimesController.java
+++ src/main/java/at/qe/skeleton/ui/controllers/BorrowTimesController.java
@@ -1,17 +1,20 @@
 package at.qe.skeleton.ui.controllers;
 
-import at.qe.skeleton.model.MediaBorrowTime;
-import at.qe.skeleton.model.MediaType;
-import at.qe.skeleton.repositories.MediaBorrowTimeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import java.io.Serializable;
+import java.util.Collection;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import java.io.Serializable;
-import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import at.qe.skeleton.model.MediaBorrowTime;
+import at.qe.skeleton.model.MediaType;
+import at.qe.skeleton.repositories.MediaBorrowTimeRepository;
+import at.qe.skeleton.services.UndoRedoService;
 
 @Component
 @Scope("view")
@@ -20,6 +23,9 @@ public class BorrowTimesController implements Serializable {
 
 	@Autowired
 	private MediaBorrowTimeRepository mediaBorrowTimeRepository;
+
+	@Autowired
+	private UndoRedoService undoRedoService;
 
 	@PostConstruct
 	public void init() {
@@ -33,27 +39,30 @@ public class BorrowTimesController implements Serializable {
 	/**
 	 * Saves the currentBorrowTime object to the database
 	 */
-	public void save(){
-		if(mediaBorrowTimes == null) return;
-
-		for(MediaBorrowTime bt : mediaBorrowTimes){
-			mediaBorrowTimeRepository.save(bt);
+	public void save() {
+		if (mediaBorrowTimes == null) {
+			return;
 		}
 
-		// TODO: add undo/redo action
+		Collection<MediaBorrowTime> currentBorrowTimes = mediaBorrowTimeRepository.findAll();
+		undoRedoService.addAction(
+				undoRedoService.createAction(currentBorrowTimes, UndoRedoService.ActionType.EDIT_MEDIA_BORROW_TIME));
+
+		for (MediaBorrowTime bt : mediaBorrowTimes) {
+			mediaBorrowTimeRepository.save(bt);
+		}
 
 		FacesContext context = FacesContext.getCurrentInstance();
 		context.addMessage("asGrowl", new FacesMessage(FacesMessage.SEVERITY_INFO, "The borrow times were edited", ""));
 	}
 
 	/**
-	 * Converts the media type into a formatted, human-readable
-	 * string (first letter uppercase, the rest lower case)
+	 * Converts the media type into a formatted, human-readable string (first letter
+	 * uppercase, the rest lower case)
 	 *
 	 * @return the converted media type
 	 */
-	public String getMediaTypeHuman(MediaType mediaType) {
-		return mediaType.toString().substring(0, 1).toUpperCase()
-				+ mediaType.toString().substring(1).toLowerCase();
+	public String getMediaTypeHuman(final MediaType mediaType) {
+		return mediaType.toString().substring(0, 1).toUpperCase() + mediaType.toString().substring(1).toLowerCase();
 	}
 }
